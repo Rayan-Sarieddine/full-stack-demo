274a1f4128f33bcebc0497d5b61a266c236338ef
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-26 18:49:03 +0200

    <frontend> implemented core helper for axios request

---
+++ frontend/src/core/helpers/request.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { local } from "./localStorage";
+
+type requestParams = {
+  route: string;
+  method: "GET" | "POST" | "DELETE" | "PUT";
+  body?: unknown;
+};
+
+axios.defaults.baseURL = "http://localhost:3000";
+export const sendRequest = async ({
+  route,
+  method = "GET",
+  body,
+}: requestParams) => {
+  const token: string | null | void = local("type");
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+  const response = await axios({ method, url: route, data: body, headers });
+  return response.data;
+};
