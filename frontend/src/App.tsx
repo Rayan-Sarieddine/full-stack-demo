f8f0bddc893f4f6d515e7e53d0c811760b3ecffd
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-26 18:59:48 +0200

    <frontend> added store to app through provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,22 @@ import SignUp from "./domain/pages/SignUp";
 import ForgetPassword from "./domain/pages/ForgetPassword";
 import ResetPassword from "./domain/pages/ResetPassword";
 import EmailVerified from "./domain/pages/EmailVerified";
+import { Provider } from "react-redux";
+import { store } from "./core/dataSource/localDataSource/store";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/forget-password" element={<ForgetPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/email-verified" element={<EmailVerified />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/email-verified" element={<EmailVerified />} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
