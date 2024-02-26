aef5caca86faa9be9214645d01979238c1c69577
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-26 18:07:40 +0200

    <frontend> implemented routing with react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./styles/index.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./domain/pages/Home";
+import Login from "./domain/pages/Login";
+import SignUp from "./domain/pages/SignUp";
+import ForgetPassword from "./domain/pages/ForgetPassword";
+import ResetPassword from "./domain/pages/ResetPassword";
+import EmailVerified from "./domain/pages/EmailVerified";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/email-verified" element={<EmailVerified />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
