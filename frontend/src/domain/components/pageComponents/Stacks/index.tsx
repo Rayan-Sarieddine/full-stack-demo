5fa28214415a5e602f235dd48a8ea3ab2c9e0fe5
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-26 23:48:59 +0200

    <frontend> implemented home page

---
+++ frontend/src/domain/components/pageComponents/Stacks/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Stacks() {
+  return (
+    <div className="stacks">
+      <div className="stacks_technologies">
+        <p>| Maxiphy Demo</p>
+        <h2>Full Stack Application</h2>
+        <h3>
+          This Full Stack app was built using Nest.js, PostgreSQL, Prisma, React
+          and Typescript.
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default Stacks;
--- frontend/src/domain/pages/Home/index.tsx
+++ frontend/src/domain/pages/Home/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./style.css";
 import Nav from "../../components/common/Nav";
 import Footer from "../../components/common/Footer";
+import Stacks from "../../components/pageComponents/Stacks";
 
 function Home() {
   return (
     <div>
       <Nav />
+      <Stacks />
       <Footer />
     </div>
   );
