7cef3d4b24836655d93571f07013cb13bb24b1b5
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-26 21:24:55 +0200

    <frontend> implemented email verified  page

--- frontend/src/domain/pages/EmailVerified/index.tsx
+++ frontend/src/domain/pages/EmailVerified/index.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 import "./style.css";
+import CheckMark from "../../components/common/CheckMark";
 
-function EmailVerified() {
-  return <div>EmailVerified</div>;
-}
+const EmailVerified = () => {
+  const navigate: NavigateFunction = useNavigate();
+
+  return (
+    <section className="email-verified">
+      <div>
+        <CheckMark message={"Email Verified Successfully!"} />
+        <div className="email-verified-login">
+          <p>
+            Back To{" "}
+            <button
+              className="email-verified-login-link"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Login
+            </button>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default EmailVerified;
