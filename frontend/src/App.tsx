b2d8cd900f5130682cd5b3e988a24ebbd01775be
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-26 17:50:41 +0200

    <frontend> imported styles to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-
+import React from "react";
+import "./styles/index.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
