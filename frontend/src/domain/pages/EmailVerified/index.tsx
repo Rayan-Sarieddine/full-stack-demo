68e857cc63c0f5fc58dfddc166f48ae8b07ae35b
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-26 18:04:35 +0200

    <frontend> implemented pages structure

---
+++ frontend/src/domain/pages/EmailVerified/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function EmailVerified() {
+  return <div>EmailVerified</div>;
+}
+
+export default EmailVerified;
---
+++ frontend/src/domain/pages/ForgetPassword/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function ForgetPassword() {
+  return <div>ForgetPassword</div>;
+}
+
+export default ForgetPassword;
