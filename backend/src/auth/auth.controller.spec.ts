a2fc20b0fc35cc0445ecf37e491dff319040d83c
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-27 22:10:01 +0200

    <backend> implemented testing for auth controller and services

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,6 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import { ExecutionContext } from '@nestjs/common';
+
+const mockAuthService = {
+  register: jest.fn((email, password, fullName, userType) => {
+    return { id: 1, email, password, fullName, userType };
+  }),
+  verifyEmail: jest.fn(),
+  login: jest.fn((user) => {
+    return { accessToken: 'test-token', ...user };
+  }),
+  sendPasswordResetEmail: jest.fn(),
+  resetPassword: jest.fn(),
+};
+
+class MockLocalAuthGuard {
+  canActivate(context: ExecutionContext): boolean {
+    return true;
+  }
+}
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,13 +28,92 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
+    })
+      .overrideGuard(LocalAuthGuard)
+      .useClass(MockLocalAuthGuard)
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should successfully register a user', async () => {
+      expect(
+        await controller.register({
+          email: 'test@test.com',
+          password: 'password',
+          fullName: 'Test User',
+          userType: 'USER',
+        }),
+      ).toEqual({
+        id: expect.any(Number),
+        email: 'test@test.com',
+        password: 'password',
+        fullName: 'Test User',
+        userType: 'USER',
+      });
+      expect(mockAuthService.register).toHaveBeenCalled();
+    });
+  });
+
+  describe('verifyEmail', () => {
+    it('should verify email and redirect', async () => {
+      const mockRes = { redirect: jest.fn() };
+      await controller.verifyEmail('token', mockRes);
+      expect(mockAuthService.verifyEmail).toHaveBeenCalledWith('token');
+      expect(mockRes.redirect).toHaveBeenCalledWith(
+        'http://localhost:3000/email-verified',
+      );
+    });
+  });
+
+  describe('login', () => {
+    it('should return access token on successful login', async () => {
+      const user = { userId: 1, email: 'test@test.com' };
+      const req = { user };
+      mockAuthService.login.mockResolvedValue({
+        accessToken: 'some-access-token',
+        ...user,
+      });
+
+      await expect(controller.login(req)).resolves.toEqual({
+        accessToken: expect.any(String),
+        userId: user.userId,
+        email: user.email,
+      });
+      expect(mockAuthService.login).toHaveBeenCalledWith(req.user);
+    });
+  });
+
+  describe('forgotPassword', () => {
+    it('should initiate password reset process', async () => {
+      const email = 'test@test.com';
+      await controller.forgotPassword(email);
+      expect(mockAuthService.sendPasswordResetEmail).toHaveBeenCalledWith(
+        email,
+      );
+    });
+  });
+
+  describe('resetPassword', () => {
+    it('should reset the password successfully', async () => {
+      const token = 'reset-token';
+      const newPassword = 'newPassword';
+      await controller.resetPassword(token, newPassword);
+      expect(mockAuthService.resetPassword).toHaveBeenCalledWith(
+        token,
+        newPassword,
+      );
+    });
   });
 });
--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,18 +1,105 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../../prisma/prisma.service';
+import { HttpException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+const mockPrismaService = () => ({
+  user: {
+    create: jest.fn(),
+    findFirst: jest.fn(),
+    findUnique: jest.fn(),
+    update: jest.fn(),
+  },
+});
+const mockJwtService = () => ({
+  sign: jest.fn(),
+});
 
 describe('AuthService', () => {
   let service: AuthService;
+  let prismaService;
+  let jwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: PrismaService, useFactory: mockPrismaService },
+        { provide: JwtService, useFactory: mockJwtService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    prismaService = module.get<PrismaService>(PrismaService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should successfully register a user', async () => {
+      prismaService.user.create.mockResolvedValue({
+        id: 'someId',
+        email: 'test@example.com',
+        fullName: 'Test User',
+        userType: 'USER',
+      });
+      expect(
+        service.register('test@example.com', 'Password1!', 'Test User', 'USER'),
+      ).resolves.not.toThrow();
+    });
+
+    it('should throw an exception if the user already exists', async () => {
+      prismaService.user.create.mockRejectedValue(
+        new HttpException('User already exists', 400),
+      );
+      await expect(
+        service.register('test@example.com', 'Password1!', 'Test User', 'USER'),
+      ).rejects.toThrow(HttpException);
+    });
+  });
+
+  describe('verifyEmail', () => {
+    it('should verify the user email', async () => {
+      prismaService.user.findFirst.mockResolvedValue({
+        id: 'someId',
+        emailVerified: false,
+      });
+      prismaService.user.update.mockResolvedValue({ emailVerified: true });
+
+      await expect(service.verifyEmail('someToken')).resolves.toBe(true);
+    });
+
+    it('should throw an exception if token is invalid', async () => {
+      prismaService.user.findFirst.mockResolvedValue(null);
+      await expect(service.verifyEmail('invalidToken')).rejects.toThrow(
+        HttpException,
+      );
+    });
+  });
+
+  describe('validateUser', () => {
+    it('should return the user if validation is successful', async () => {
+      prismaService.user.findUnique.mockResolvedValue({
+        id: 545454,
+        email: 'test@example.com',
+        password: await bcrypt.hash('Password1!', 10),
+        emailVerified: true,
+      });
+
+      await expect(
+        service.validateUser('test@example.com', 'Password1!'),
+      ).resolves.not.toBeNull();
+    });
+
+    it('should return null if user does not exist', async () => {
+      prismaService.user.findUnique.mockResolvedValue(null);
+      await expect(
+        service.validateUser('nonexistent@example.com', 'Password1!'),
+      ).resolves.toBeNull();
+    });
   });
 });
