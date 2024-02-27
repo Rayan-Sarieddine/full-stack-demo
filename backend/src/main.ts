3bb4e0fa4dd3099c54b0877c304237dad12207c6
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-27 18:17:41 +0200

    <backend> updated main configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  await app.listen(3001);
 }
 bootstrap();
