ceefebfce80160f0312790671c210252f5ce7d6a
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-02-27 19:10:21 +0200

    <backend> implemented auth register and login controller functions

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Post('register')
+  async register(@Body() body: any) {
+    const userType = body.userType || 'USER';
+    return this.authService.register(
+      body.email,
+      body.password,
+      body.fullName,
+      userType,
+    );
+  }
+  @Get('verify')
+  async verifyEmail(@Query('token') token: string, @Res() res: any) {
+    try {
+      await this.authService.verifyEmail(token);
+      // Redirect or respond as needed
+      res.redirect('http://localhost:3000/email-verified');
+    } catch (error) {
+      res.status(400).send('Email verification failed');
+    }
+  }
+  @Post('login')
+  @HttpCode(200)
+  async login(@Req() req) {
+    return this.authService.login(req.user);
+  }
 }
