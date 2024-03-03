84d49598321c31155f66251a08881eab6a6be128
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2024-03-03 12:22:09 +0200

    <frontend> implemented Todo component structure

--- frontend/src/domain/components/pageComponents/Todo/index.tsx
+++ frontend/src/domain/components/pageComponents/Todo/index.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./style.css";
 
-function Todo({ title, description, priorityLevel, completed, pinned }) {
-  return <div className="todo">Todo</div>;
+export type PriorityLevel = "TOP" | "MEDIUM" | "AVERAGE";
+
+export type Todo = {
+  title: string;
+  description?: string;
+  date: Date;
+  priorityLevel: PriorityLevel;
+  completed: boolean;
+  pinned: boolean;
+};
+
+function Todo({
+  title,
+  description,
+  priorityLevel,
+  date,
+  completed,
+  pinned,
+}: Todo) {
+  return (
+    <div className="todo">
+      <div className="rows">
+        <div className="row1">
+          <input type="text" placeholder={title} />
+          <div className="row1-buttons">
+            <button>pin</button>
+            <button>complete</button>
+          </div>
+        </div>
+        <div className="row2">
+          <input type="text" placeholder={description} />
+          <div className="row2-buttons">
+            <button>edit</button>
+            <button>delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
--- frontend/src/domain/pages/Home/index.tsx
+++ frontend/src/domain/pages/Home/index.tsx
@@ -3,12 +3,21 @@ import "./style.css";
 import Nav from "../../components/common/Nav";
 import Footer from "../../components/common/Footer";
 import Stacks from "../../components/pageComponents/Stacks";
+import Todo from "../../components/pageComponents/Todo";
 
 function Home() {
+  const dateObject = new Date("2024-11-11T00:00:00");
   return (
     <div className="home">
       <Nav />
-      <Stacks />
+      <Todo
+        title="hello"
+        description="descr"
+        pinned={false}
+        completed={false}
+        date={dateObject}
+        priorityLevel="TOP"
+      />
       <Footer />
     </div>
   );
